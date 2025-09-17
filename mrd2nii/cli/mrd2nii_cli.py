3ce26ec067ea6ac6323df670bfde3550ee2b5236
Alexandre D'Astous <po09i@hotmail.com>
2025-09-17 16:50:20 -0400

    Add ability to specify a file rather than a folder

--- mrd2nii/cli/mrd2nii_cli.py
+++ mrd2nii/cli/mrd2nii_cli.py
@@ -16,14 +16,22 @@ logger = logging.getLogger(__name__)
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('-i', '--input', 'path_mrd', type=click.Path(), required=True,
-              help="Input path to MRD folder")
+              help="Input path to MRD folder/file")
 @click.option('-o', '--output', 'path_output', type=click.Path(), required=True,
               help="Path to output folder")
 # @click.option('-v', '--verbose', type=click.Choice(['info', 'debug']), default='info',
 #               help="Be more verbose")
 def mrd2nii_int(path_mrd, path_output):
+    if not os.path.exists(path_mrd):
+        raise FileNotFoundError(f"Output folder does not exist: {path_mrd}")
 
-    list_files = os.listdir(path_mrd)
+    if os.path.isdir(path_mrd):
+        list_files = os.listdir(path_mrd)
+    elif os.path.isfile(path_mrd):
+        list_files = [path_mrd]
+    else:
+        raise ValueError(f"Input path is neither a folder nor a file: {path_mrd}")
+
     for file in list_files:
         if not os.path.splitext(file)[1] in [".mrd", ".h5"]:
             continue
